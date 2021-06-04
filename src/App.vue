898fdaa51b8d17a4ba93d5f45de7666bec02f505
Mohamed Hanini <mohamed.hanini95@gmail.com>
2021-06-04 13:09:15 +0100

    creating the header component

--- src/App.vue
+++ src/App.vue
@@ -6,35 +6,8 @@
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <TheHeadline :headline="headline" />
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
@@ -44,12 +17,18 @@
 </template>
 
 <script>
-
+import TheHeadline from './components/TheHeadline'
 export default {
   name: 'App',
-
+  components:{
+    TheHeadline
+  },
+  computed:{
+    headline(){
+      return"Front-end Coding Challenge"
+    }
+  },
   data: () => ({
-    //
   }),
 };
 </script>
---
+++ src/components/TheHeadline.vue
@@ -0,0 +1,14 @@
+<template>
+    <span> {{ headline }} </span>
+</template>
+<script>
+export default {
+    name:"TheHeadline",
+    props:{        
+        headline:{
+            type:String,
+            require:true
+        }
+        }
+    }
+</script>
