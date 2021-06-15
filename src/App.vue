27ea1cb3345e1e52eccc787d5e77898fc800605a
Mohamed Hanini <mohamed.hanini95@gmail.com>
2021-06-15 10:18:38 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <v-app>
     <TheHeader />
-
     <v-main>
       <router-view/>
     </v-main>
--- src/components/TheDescription.vue
+++ src/components/TheDescription.vue
@@ -6,7 +6,7 @@ export default {
     name:'TheDescription',
     props:{
         description:{
-            type:Text
+            type:String
         }
     }
 }
